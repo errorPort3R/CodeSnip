2cb2df5a2f8545fb4afae49b87f79fb7e67365e2
jeffryporter <jporter7@gmail.com>
2016-08-29 16:26:50 -0400

    initial commit

---
+++ src/main/java/com/jporter7industries/CodeSnipApplication.java
@@ -0,0 +1,12 @@
+package com.jporter7industries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSnipApplication.class, args);
+	}
+}
